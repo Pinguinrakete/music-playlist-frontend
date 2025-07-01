92a196c488c778baae01cd9a776db6f190fa82b3
Pinguinrakete <Pinguinrakete@gmail.com>
2025-07-01 21:39:02 +0200

    add function logInSubmit

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const postUrl = "http://127.0.0.1:8000/api/endpoint/";
+const GET_URL = "http://127.0.0.1:8000/api/endpoint/";
+const POST_URL = "http://127.0.0.1:8000/api/endpoint/";
+const LOGIN_URL = "http://127.0.0.1:8000/api/auth/login/";
+
+let apiKey = '';
+
 
 async function post() {
-    const interpret = document.getElementById('interpret').value;
-    const title = document.getElementById('title').value;
-
     const data = {
-        interpret: interpret,
-        title: title
+        interpret: document.getElementById('interpret').value,
+        title: document.getElementById('title').value
     };
 
-    let response = await fetch(postUrl, {
+    let response = await fetch(POST_URL, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -17,13 +19,58 @@ async function post() {
         body: JSON.stringify(data)
         });
 
-        let result = await response.json();
-        console.log(result);
-
+    const result = await response.json();
+    console.log("Antwort vom Server:", result);
     cleanInputfield();
 }
+
+
+async function get() {
+    try {
+        const response = await fetch(GET_URL);
+        const data = await response.json();
+        renderPlaylist(data);
+    } catch (error) {
+        console.error('Fehler beim Abrufen der Daten:', error);
+    }
+}
+
 
 function cleanInputfield() {
     document.getElementById("interpret").value = "";
     document.getElementById("title").value = "";
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
+}
+
+
+function renderPlaylist(data) {
+    document.getElementById("output-database").innerHTML = '';
+
+    for(let i = 0; i < data.length; i++) {
+        document.getElementById("output-database").innerHTML += `interpret: ${data[i]['interpret']} title: ${data[i]['title']} <br>`;
+    }
+}
+
+
+async function logInSubmit() {
+    const data = {
+        username: document.getElementById('username').value,
+        password: document.getElementById('password').value
+    };
+
+    console.log('post', data)
+    let request = await fetch(LOGIN_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+        });
+
+    const response = await request.json();
+    console.log("Antwort vom Server:", response);
+    apiKey = response.token;
+    console.log('Api-Key', apiKey);
+    cleanInputfield();
 }
